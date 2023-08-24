ede789606bd33b34d7f0196583e44063ea60e27f
OyewoleOyedeji <57059138+OyewoleOyedeji@users.noreply.github.com>
2023-08-24 08:45:53 +0100

    chore(devDependencies): Bumped [@types/node, changelogen, @nuxt/devtools, +1]
    refactor: Removed redundant async function (setup.ts)

--- setup.ts
+++ setup.ts
@@ -7,32 +7,30 @@ import {
 } from "fs";
 import { join } from "path";
 
-(async () => {
-  const paths = {
-    icons: join("./node_modules/bootstrap-icons/icons"),
-    components: join("./src/runtime/components"),
-  };
+const paths = {
+  icons: join("./node_modules/bootstrap-icons/icons"),
+  components: join("./src/runtime/components"),
+};
 
-  if (!existsSync(paths.components)) mkdirSync(paths.components);
+if (!existsSync(paths.components)) mkdirSync(paths.components);
 
-  const bootstrapIcons = readdirSync(paths.icons) as string[];
+const bootstrapIcons = readdirSync(paths.icons) as string[];
 
-  for (const icon of bootstrapIcons) {
-    writeFileSync(
-      join(
-        paths.components,
-        icon
-          .split("-")
-          .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
-          .join("")
-          .replace(".svg", ".vue")
-      ),
-      `<template>
+for (const icon of bootstrapIcons) {
+  writeFileSync(
+    join(
+      paths.components,
+      icon
+        .split("-")
+        .map((word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+        .join("")
+        .replace(".svg", ".vue")
+    ),
+    `<template>
   ${readFileSync(join(paths.icons, icon)).toString()}
 </template>`,
-      { encoding: "utf-8" }
-    );
-  }
+    { encoding: "utf-8" }
+  );
+}
 
-  console.log(`[INFO] Done generating icons from ${paths.icons}`);
-})();
+console.log(`[INFO] Done generating icons from ${paths.icons}`);
