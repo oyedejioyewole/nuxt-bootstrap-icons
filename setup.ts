b3c4504c96a685e0e2376e895f68e1dfc314914f
OyewoleOyedeji <57059138+OyewoleOyedeji@users.noreply.github.com>
2024-01-13 19:25:54 +0100

    fix: Refactor faulty setup script

--- setup.ts
+++ setup.ts
@@ -1,5 +1,6 @@
-import { existsSync, mkdirSync, readFile, readdirSync, writeFile } from "fs";
-import { dirname, join, resolve } from "path";
+import { existsSync } from "fs";
+import { mkdir, open, readFile, readdir, writeFile } from "fs/promises";
+import { dirname, join, parse, resolve } from "path";
 import { fileURLToPath } from "url";
 
 // Resolve path to this file
@@ -15,57 +16,44 @@ const paths = {
   template: resolve(__dirname, "./src/runtime/templates/_icon.vue"),
 };
 
-// Create components directory if it doesn't exist
-if (!existsSync(paths.components)) mkdirSync(paths.components);
+async function setup() {
+  // Create components directory if it doesn't exist
+  if (!existsSync(paths.components)) await mkdir(paths.components);
+  else if (!existsSync(paths.icons.list))
+    await open(join(paths.icons.list), "w");
 
-const rawIcons: string[] = readdirSync(paths.icons.source);
+  const rawIcons: string[] = await readdir(paths.icons.source);
 
-// Get icon name without extension
-const iconList = rawIcons.map((icon) => icon.replace(".svg", ""));
+  // Get icon name without extension
+  const iconList = rawIcons.map((icon) => parse(icon).name);
 
-// Convert icon names to PascalCase
-const componentList = iconList
-  .map((icon) =>
-    icon
-      .split("-")
-      .map((word) => word.at(0)?.toUpperCase() + word.slice(1))
-      .join(""),
-  )
-  .map((icon) => icon + ".vue");
+  // Convert icon names to PascalCase
+  const componentList = iconList.map(
+    (icon) =>
+      icon
+        .split("-")
+        .map((word) => word.at(0)?.toUpperCase() + word.slice(1))
+        .join("") + ".vue",
+  );
 
-// Write icons from source to a list
-writeFile(
-  paths.icons.list,
-  JSON.stringify(iconList),
-  { encoding: "utf-8" },
-  (error) => {
-    if (error) throw error;
-  },
-);
+  // Write icons from source to a list
+  await writeFile(paths.icons.list, JSON.stringify(iconList));
 
-// Read the template
-readFile(paths.template, (error, data) => {
-  if (error) throw error;
-
-  const template = data.toString();
+  // Read the template
+  const template = (await readFile(paths.template)).toString();
 
   // For each icon, read the source file and write it to the component
-  rawIcons.forEach((icon, index) => {
-    readFile(join(paths.icons.source, icon), (error, data) => {
-      if (error) throw error;
+  rawIcons.forEach(async (icon, index) => {
+    const content = (await readFile(join(paths.icons.source, icon))).toString();
 
-      const content = data.toString();
-
-      writeFile(
-        join(paths.components, componentList[index]),
-        template.replace("$", content),
-        { encoding: "utf-8" },
-        (error) => {
-          if (error) throw error;
-        },
-      );
-    });
+    await writeFile(
+      join(paths.components, componentList[index]),
+      template.replace("$", content),
+    );
   });
-});
 
-console.info("[INFO] Icons have been generated!");
+  console.info("[INFO]: 🎉 Icons have been generated!");
+}
+
+console.info("[INFO]: ✨ Setting up icons");
+setup();
