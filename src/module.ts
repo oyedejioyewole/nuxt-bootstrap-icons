66e0cdcff5bf4b7b66bd67aade5a4ac4cfd6ac2a
Daniel Roe <daniel@roe.dev>
2024-06-14 14:08:10 +0100

    chore: indicate compatibility with new v4 major

--- src/module.ts
+++ src/module.ts
@@ -44,7 +44,7 @@ export default defineNuxtModule<ModuleOptions>({
     name: "nuxt-bootstrap-icons",
     configKey: "bootstrapIcons",
     compatibility: {
-      nuxt: "^3.0.0",
+      nuxt: ">=3.0.0",
     },
   },
   // Default configuration options of the Nuxt module
